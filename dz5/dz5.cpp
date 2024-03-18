99cd4eec4be02cc9f5052ecf54b1073636bea884
GERMETKER <leha.usanov.0808@mail.ru>
2024-03-18 18:30:33 +0500

    Добавьте файлы проекта.

---
+++ dz5/dz5.cpp
@@ -0,0 +1,37 @@
+﻿#include <Windows.h>
+#include <iostream>
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    for (int i = 9; i > -1; i--)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (j == 0 || j == 10)
+            {
+                std::cout << "   ";
+            }
+            else if (i <= 8 && i >= 1)
+            {
+                std::cout << i << " ";
+            }
+            else if (i == 9 || i == 0)
+            {
+                std::cout << "no";
+            }
+            if (j >= 0 && j <= 10)
+            {
+
+            }
+            else
+            {
+                std::cout << i << " ";
+            }
+        }
+        
+        std::cout << "\n";
+    }
+    
+    return 0;
+}
